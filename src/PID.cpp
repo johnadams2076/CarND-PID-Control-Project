839c814fd608346b97b51609ab2342043548e150
johnadams2076 <dronham@nym.hush.com>
2019-05-10 19:12:52 -0500

    Initial Commit

---
+++ src/PID.cpp
@@ -0,0 +1,44 @@
+#include "PID.h"
+
+/**
+ * TODO: Add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * Initialize PID coefficients (and errors, if needed)
+   */
+
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
+
+	Kp = Kp_;
+	Ki = Ki_;
+	Kd = Kd_;
+
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * Update PID errors based on cte.
+   */
+
+	d_error = (cte - p_error);
+
+	p_error = cte;
+
+	i_error += cte;
+
+}
+
+double PID::TotalError() {
+  /**
+   *  Calculate and return the total error
+   */
+	return -Kp * p_error - Kd * d_error - Ki * i_error; //Total error calc !
+}
